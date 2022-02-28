e244567d5276ab797fc022db089592e2340ece49
imranahmed1990 <imranmarin1990@gmail.com>
2022-03-01 03:53:42 +0600

    2nd commit

--- js/app.js
+++ js/app.js
@@ -14,20 +14,24 @@ const loadSearchText = () => {
 const displayPhone = (phones) => {
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
+        if (!phones) {
+            return alert('please type again')
+        }
         const div = document.createElement('div');
         div.classList.add('col');
+
         div.innerHTML = `
         <div class="card h-100">
         <img style="width:200px;" src=${phone.image} class="card-img-top img-fluid mx-auto p-3" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to show that equal
-                height action.</p>
+            <h5 class="card-title"> Name:${phone.phone_name}</h5>
+            <p class="card-text">Brand:${phone.brand}</p>
+            <p> Price:${phone.slug} </p>
         </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
+        <div class="d-grid gap-2 col-6 mx-auto py-2">
+  <button class="btn btn-primary" type="button">More Info</button>
+  
+</div>
     </div>
         `;
         searchResult.appendChild(div);
