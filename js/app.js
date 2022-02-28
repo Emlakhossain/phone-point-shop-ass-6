484fb5bb5ee6334a81658d814f626ba7520f2bb7
imranahmed1990 <imranmarin1990@gmail.com>
2022-03-01 04:42:25 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -22,14 +22,14 @@ const displayPhone = (phones) => {
 
         div.innerHTML = `
         <div class="card h-100">
-        <img style="width:200px;" src=${phone.image} class="card-img-top img-fluid mx-auto p-3" alt="...">
+        <img style="width:200px;" src="${phone.image} "class="card-img-top img-fluid mx-auto p-3" alt="...">
         <div class="card-body">
             <h5 class="card-title"> Name:${phone.phone_name}</h5>
             <p class="card-text">Brand:${phone.brand}</p>
-            <p> Price:${phone.slug} </p>
+            <p> Id::${phone.slug} </p>
         </div>
         <div class="d-grid gap-2 col-6 mx-auto py-2">
-  <button class="btn btn-primary" type="button">More Info</button>
+  <button onclick="moreInformation('${phone.slug}')" class="btn btn-primary" type="button">More Info</button>
   
 </div>
     </div>
@@ -37,6 +37,32 @@ const displayPhone = (phones) => {
         searchResult.appendChild(div);
 
     })
+};
+
+const moreInformation = (Id) => {
+    console.log(Id)
+    const url = `https://openapi.programming-hero.com/api/phone/${Id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMoreInformation(data.data))
+
+
+
+}
+const displayMoreInformation = (phone) => {
+    console.log(phone)
+    const moreInfo = document.getElementById('more-info');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+<img style="width:200px;" src="${phone.image}" class="card-img-top img-fluid mx-auto p-3" alt="...">
+<div class="card-body">
+    <h5 class="card-title">Name: ${phone.phone_name}</h5>
+    <p class="card-text">ReliseDate: ${phone.releaseDate}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+`;
+    moreInfo.appendChild(div)
 }
 
 
