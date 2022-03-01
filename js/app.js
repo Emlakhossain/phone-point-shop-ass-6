1ee5eb1a87995267ee32033feae83516ba123332
imranahmed1990 <imranmarin1990@gmail.com>
2022-03-02 00:23:03 +0600

    another commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,37 @@
 /*
 load search text 
 */
-const loadSearchText = () => {
-    const searchField = document.getElementById('search-field'); const searchText = searchField.value;
-    searchField.value = '';
+// spinner function
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+// Search field setup by arrow function
+const loadSearchField = () => {
+    const searchText = document.getElementById('search-field').value;
+    // display spinner
+    toggleSpinner('block')
+    loadSearchData(searchText);
+    document.getElementById('search-field').value = '';
+}
+
+
+const loadSearchData = searchText => {
+
     // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
 }
+loadSearchData('phone')
 
 const displayPhone = (phones) => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     phones.forEach(phone => {
-        if (!phones) {
-            return alert('please type again')
-        }
+        // if (!phones) {
+        //     return alert('please type again')
+        // }
         const div = document.createElement('div');
         div.classList.add('col');
 
@@ -25,19 +40,21 @@ const displayPhone = (phones) => {
         <img style="width:200px;" src="${phone.image} "class="card-img-top img-fluid mx-auto p-3" alt="...">
         <div class="card-body">
             <h5 class="card-title"> Name:${phone.phone_name}</h5>
-            <p class="card-text">Brand:${phone.brand}</p>
-            <p> Id::${phone.slug} </p>
+            <p>Brand:${phone.brand}</p>
+            <p>Id:${phone.slug} </p>
         </div>
-        <div class="d-grid gap-2 col-6 mx-auto py-2">
+        <div class="d-grid gap-2 col-6 mx-auto py-3">
   <button onclick="moreInformation('${phone.slug}')" class="btn btn-primary" type="button">More Info</button>
   
 </div>
     </div>
         `;
         searchResult.appendChild(div);
+    });
+    toggleSpinner('none');
 
-    })
-};
+}
+
 
 const moreInformation = (Id) => {
     console.log(Id)
@@ -50,7 +67,7 @@ const moreInformation = (Id) => {
 
 }
 const displayMoreInformation = (phone) => {
-    console.log(phone)
+    // console.log(phone)
     const moreInfo = document.getElementById('more-info');
     const div = document.createElement('div');
     div.classList.add('card');
